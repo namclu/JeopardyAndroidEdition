47ecf50133a6b8c99bdff498426a7db38479df69
namclu <namlu1205@gmail.com>
2017-01-10 15:05:46 +1100

    2 add TextWatcher, EditText reference for question three, add method afterTextChanged() to verify if text input is correct. Confirm that new EditText field works

--- app/src/main/java/com/example/namlu/jeopardyandroidedition/MainActivity.java
+++ app/src/main/java/com/example/namlu/jeopardyandroidedition/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.namlu.jeopardyandroidedition;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener,
-        RadioGroup.OnCheckedChangeListener, View.OnClickListener{
+        RadioGroup.OnCheckedChangeListener, View.OnClickListener, TextWatcher{
 
     static final int NUM_OF_QUESTIONS = 4;
 
@@ -21,12 +25,16 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     int correctResponses = 0;
     boolean questionOnePointsAwarded = false;
     boolean questionTwoPointsAwarded = false;
+    boolean questionThreePointsAwarded = false;
 
     // Reference to RadioButton
     RadioGroup questionOneGroup;
 
     // Reference to CheckBoxes
     CheckBox questionTwoA, questionTwoB, questionTwoC, questionTwoD;
+
+    // Reference to EditTextBoxes
+    EditText questionThree;
 
     // Reference to Button
     Button submitButton;
@@ -53,11 +61,16 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         questionTwoD = (CheckBox) findViewById(R.id.cb_question_two_d);
         questionTwoD.setOnCheckedChangeListener(this);
 
+        // Reference to question 3
+        questionThree = (EditText) findViewById(R.id.et_question_three);
+        questionThree.addTextChangedListener(this);
+
         // Listener for Submit button
         submitButton = (Button) findViewById(R.id.button_submit);
         submitButton.setOnClickListener(this);
     }
 
+    // Do something when CheckBoxes are ticked
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
         switch (compoundButton.getId()) {
@@ -75,6 +88,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         }
     }
 
+    // Do something when RadioBoxes are ticked
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, int checkedButtonId) {
         // If response is correct, set points awarded to true, else set to false
@@ -90,22 +104,51 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         }
     }
 
+    // Do something when Button is pressed
     @Override
     public void onClick(View view) {
-        // When user clicks on 'Submit', present Toast message with user's score
+        // When user clicks on 'Submit', calculate points and present Toast message with user's score
         if (view.getId() == R.id.button_submit) {
-            correctResponses = 0;
             awardPoints();
             Toast.makeText(this, "Answers correct: " + correctResponses + "/" + NUM_OF_QUESTIONS, Toast.LENGTH_SHORT).show();
         }
     }
 
+    // Do something when EditText fields are used
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        String questionThreeString = questionThree.getText().toString();
+        if (!TextUtils.isEmpty(questionThreeString)) {
+            if (questionThreeString.equalsIgnoreCase("activity")) {
+                questionThreePointsAwarded = true;
+            } else {
+                questionThreePointsAwarded = false;
+            }
+        }
+    }
+
     // Awards points based on number of correct responses
     void awardPoints() {
+        // Reset correct responses to zero
+        correctResponses = 0;
+
         if (questionOnePointsAwarded) {
             correctResponses++;
         }
         if (questionTwoPointsAwarded) {
+            correctResponses++;
+        }
+        if (questionThreePointsAwarded) {
             correctResponses++;
         }
     }
